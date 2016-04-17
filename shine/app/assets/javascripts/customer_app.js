f49757eea1df6edd38fb4683985e6af2ba020d5e
Ketse <snunleys@gmail.com>
2016-04-16 20:11:37 -0400

    Chapter 5 done

--- shine/app/assets/javascripts/customer_app.js
+++ shine/app/assets/javascripts/customer_app.js
@@ -1,9 +1,36 @@
 var app = angular.module('customers', []);
 
-var CustomerSearchController = function($scope) {
-  $scope.search = function(searchTerm) {
-    $scope.searchedFor = searchTerm;
-  };
-};
+app.controller("CustomerSearchController", ["$scope", "$http",
+  function($scope, $http) {
+    $scope.customers = [];
+    var page = 0;
+    $scope.search = function(searchTerm) {
+      if (searchTerm.length < 3) {
+        return;
+      }
+      $http.get("/customers.json", {
+        "params": {
+          "keywords": searchTerm,
+          "page": page
+        }
+      }).then(function(response) {
+        $scope.customers = response.data;
+      }, function(response) {
+        alert("there was a problem: " + response.status);
+      });
+      $scope.searchedFor = searchTerm;
+    };
 
-app.controller("CustomerSearchController", ["$scope", CustomerSearchController]);
+    $scope.previousPage = function() {
+      if (page > 0) {
+        page = page - 1;
+        $scope.search($scope.keywords);
+      }
+    };
+
+    $scope.nextPage = function() {
+      page = page + 1;
+      $scope.search($scope.keywords);
+    };
+  }
+]);
